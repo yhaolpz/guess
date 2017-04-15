65a6623ccc15dbe34ed7a1e0cb13ddf9a4a910d8
yhao <yhaowa@outlook.com>
2017-04-15 23:08:25 +0800

    仿ios风格 EditPersonalDataActivity

--- guess/src/main/java/com/example/asus/activity/EditPersonalDataActivity.java
+++ guess/src/main/java/com/example/asus/activity/EditPersonalDataActivity.java
@@ -1,13 +1,40 @@
 package com.example.asus.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class EditPersonalDataActivity extends AppCompatActivity {
+import com.example.asus.bmobbean.User;
+import com.example.asus.common.BaseApplication;
+import com.example.asus.common.MySwipeBackActivity;
+
+public class EditPersonalDataActivity extends MySwipeBackActivity {
+    private TextView mName;
+    private TextView mEmail;
+    private TextView mSex;
+    private TextView mAge;
+    private TextView mCity;
+    private BaseApplication mApplication;
+    private User mCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_personal_data);
+        mApplication = (BaseApplication) getApplication();
+        mCurrentUser = mApplication.getUser();
+        initView();
+    }
+
+    private void initView() {
+        mName = (TextView) findViewById(R.id.name);
+        mEmail = (TextView) findViewById(R.id.email);
+        mSex = (TextView) findViewById(R.id.sex);
+        mAge = (TextView) findViewById(R.id.age);
+        mCity = (TextView) findViewById(R.id.city);
+        mName.setText(mCurrentUser.getName());
+        mCity.setText(mCurrentUser.getCity());
+        mAge.setText(mCurrentUser.getAge() == null ? "" : mCurrentUser.getAge() + "岁");
+        mEmail.setText(mCurrentUser.getEmail());
+        mSex.setText(mCurrentUser.getSex());
     }
 }
